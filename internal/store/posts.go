26a38d6708a4cb87820cae074953313df1e1586e
Prashwar <prashwar.mishra@jiostar.com>
2025-01-08 22:12:04 +0530

    add User and Post type and add query to create User and Post

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,35 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
+
+	"github.com/lib/pq"
 )
 
 type PostsStorage struct {
 	db *sql.DB
 }
 
-func (s *PostsStorage) Create(context context.Context) error {
-	return nil
+type Post struct {
+	ID int64 `json:"id"`
+	Title string `json:"title" binding:"required"`
+	Content string	`json:"content" binding:"required"`
+	Tags []string `json:"tags"`
+	UserId int64 `json:"user_id" binding:"required"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time	`json:"updated_at"`
+}
+
+func (s *PostsStorage) Create(context context.Context, post *Post) error {
+	query := `
+				INSERT into posts 
+				(title, content, tags, user_id) 
+				values (?, ?, ?, ?) 
+				RETURNING id, created_at, updated_at
+			`
+	row := s.db.QueryRowContext(context, query, post.Title, post.Content, pq.Array(post.Tags), post.UserId)
+
+	err := row.Scan(&post.ID, &post.CreatedAt, &post.UpdatedAt)
+
+	return err
 }
--- internal/store/users.go
+++ internal/store/users.go
@@ -3,12 +3,33 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
 )
 
 type UsersStorage struct {
 	db *sql.DB
 }
 
-func (s *UsersStorage) Create(context context.Context) error {
-	return nil
+type User struct {
+	ID int64 `json:"id"`
+	Email string `json:"email" binding:"required"`
+	UserName string `json:"username"`
+	Password string `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (s *UsersStorage) Create(context context.Context, user *User) error {
+	query := `
+		INSERT INTO users (
+		email, username, password,
+		) values (?, ?, ?) RETURNING
+		id, created_at, updated_at
+	`
+
+	row := s.db.QueryRowContext(context, query, user.Email, user.UserName, user.Password)
+
+	err := row.Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
+
+	return err
 }
